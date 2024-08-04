ee21ca9768ed27bb9052a14f51054a3cc004cd69
Dylan <dylanalifu@Dylan.local>
2024-08-03 21:49:34 -0700

    movie lists added

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,7 +8,7 @@ const MovieList = ({ title, movies }) => {
     <div className="px-4">
       <h1 className="text-3xl py-4 text-white">{title}</h1>
       <div className="flex overflow-x-scroll no-scrollbar">
-        <div className="flex gap-2">
+        <div className="flex gap-3">
           {movies.map((movie) => (
             <MovieCard key={movie.id} posterPath={movie.poster_path} />
           ))}
--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -6,19 +6,21 @@ const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
 
   return (
-    <div className="-mt-[350px] relative z-10 bg-black secondary-container">
-      {movies.nowPlayingMovies && (
-        <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
-      )}
-      {movies.topRatedMovies && (
-        <MovieList title="Top Rated" movies={movies.topRatedMovies} />
-      )}
-      {movies.upcomingMovies && (
-        <MovieList title="Upcoming" movies={movies.upcomingMovies} />
-      )}
-      {movies.popularMovies && (
-        <MovieList title="Popular" movies={movies.popularMovies} />
-      )}
+    <div className="bg-black w-screen">
+      <div className="-mt-[350px] relative z-10 px-16">
+        {movies.nowPlayingMovies && (
+          <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
+        )}
+        {movies.topRatedMovies && (
+          <MovieList title="Top Rated" movies={movies.topRatedMovies} />
+        )}
+        {movies.upcomingMovies && (
+          <MovieList title="Upcoming" movies={movies.upcomingMovies} />
+        )}
+        {movies.popularMovies && (
+          <MovieList title="Popular" movies={movies.popularMovies} />
+        )}
+      </div>
     </div>
   );
 };
